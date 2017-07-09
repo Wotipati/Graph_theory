24ed3e09afa8d7862a86c8b989bb1d4bf39e668f
kayukawa-seita <k940805k.lab@gmail.com>
2017-07-09 20:43:09 +0900

    drawing graph

--- Kirchhoff's_theorem/kirchhoff.py
+++ Kirchhoff's_theorem/kirchhoff.py
@@ -1,4 +1,6 @@
 import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
 import argparse
 import math
 
@@ -10,30 +12,29 @@ if __name__ == '__main__':
 
     n = args.dim_fn
 
-    #隣接行列Aを作成
-    #乱数でn次元正方行列を作成
+    # 隣接行列Aを作成
+    # 乱数でn次元正方行列を作成
     A = np.random.rand(n, n)
 
-    #対称行列に
+    # 対称行列に
     A = np.dot(A, A.T)
 
-    #各成分を0or1に
+    # 各成分を0or1に
     A[A<1] = 0
     A[A>=1] = 1
 
-    #対各成分を0に(隣接行列作成完了)
+    # 対各成分を0に(隣接行列作成完了)
     B = np.diag(A)
     C = np.diag(B)
     A = A - C
     print("Adjacency matrix")
     print(A)
 
-
-    #ノードの次数を対角成分にもつ行列Dを作成
+    # ノードの次数を対角成分にもつ行列Dを作成
     D = np.sum(A, axis=1)
     D = np.diag(D)
 
-    #ラプラシアン行列を作成
+    # ラプラシアン行列を作成
     H = D - A
     print("Laplacian matrix")
     print(H)
@@ -41,12 +42,36 @@ if __name__ == '__main__':
 
     print("cofactors")
     for i in range(n):
-        for j in range(n):
-            #Hからi行j列を取り除いた小行列(minor)を作成
+        for j in range(i, n):
+            # Hからi行j列を取り除いた小行列(minor)を作成
             Minor = np.delete(H, i, axis=0)
             Minor = np.delete(Minor, j, axis=1)
 
-            #余因子(cofactor)を計算
+            # 余因子(cofactor)を計算
             Cofactor = ((-1)**(i+j)) * np.linalg.det(Minor)
 
             print(Cofactor)
+
+
+    # 無向グラフの作成
+    # 空のグラフの作成
+    Graph = nx.Graph()
+
+    #ノードの追加
+    for i in range(n):
+        Graph.add_node('{0}'.format(i))
+
+    # エッジの追加
+    for i in range(n):
+        for j in range(n):
+            if int(A[i][j]) == 1:
+                Graph.add_edge('{0}'.format(i), '{0}'.format(j))
+
+
+    # グラフの描画
+    plt.figure(figsize = (5,5))
+    pos = nx.spring_layout(Graph)
+    nx.draw_networkx(Graph, pos)
+
+    plt.axis("off")
+    plt.show()
