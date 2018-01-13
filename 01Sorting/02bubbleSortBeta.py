77b4550921c5c269bb8278a32a866efdc11fc938
Wotipati <k940805k.lab@gmail.com>
2018-01-13 21:54:04 +0900

    add bubbleSortBeta

---
+++ 01Sorting/02bubbleSortBeta.py
@@ -0,0 +1,40 @@
+import numpy as np
+import argparse
+
+
+def bubble_sort(input_list):
+    total_number = len(input_list)
+    flag = True
+    finished = 0
+
+    while flag:
+        flag = False
+        for i in range(total_number-1-finished):
+            if input_list[i] > input_list[i+1]:
+                stack = input_list[i]
+                input_list[i] = input_list[i+1]
+                input_list[i+1] = stack
+                flag = True
+
+        finished += 1
+        print(finished)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--n', type=int, default=8)
+    args = parser.parse_args()
+
+    total_number = args.n
+
+    bubble = np.random.randint(10, total_number*10, total_number)
+
+    print('Before:{0}'.format(bubble))
+
+    bubble_sort(bubble)
+
+    print('After: {0}'.format(bubble))
+
+
+if __name__ == '__main__':
+    main()
